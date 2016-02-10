d38331ad0976ce993e34116fe0517329577ba3cb
Maneesh Tewani <mtewani@purdue.edu>
2016-02-10 16:36:16 -0500

    Yet another bio addition
    Fixed smooth scroll

--- scroll.js
+++ scroll.js
@@ -12,7 +12,7 @@ $(function() {
     });
     $('a').click(function() {
         $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top - 50
         }, 500);
         return false;
     });
