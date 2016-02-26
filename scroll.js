93afe2e7a596673499e10b167009faf3b5fe0430
Maneesh Tewani <mtewani@purdue.edu>
2016-02-26 10:17:43 -0500

    Added the document.ready function

--- scroll.js
+++ scroll.js
@@ -1,19 +1,19 @@
-$(function() {
-    $(window).scroll(function() {
-        if($(this).scrollTop() !== 0) {
-            var nav = $(".navbar");
-            nav.css("background-color", "white");
-            $("a.white").removeClass("white").addClass("gray");
-        } else {
-            var nav = $(".navbar");
-            nav.css("background-color", "transparent");
-            $("a.gray").removeClass("gray").addClass("white");
-        }
+$(document).ready(function() {
+        $(window).scroll(function() {
+            if($(this).scrollTop() !== 0) {
+                var nav = $(".navbar");
+                nav.css("background-color", "white");
+                $("a.white").removeClass("white").addClass("gray");
+            } else {
+                var nav = $(".navbar");
+                nav.css("background-color", "transparent");
+                $("a.gray").removeClass("gray").addClass("white");
+            }
+        });
+        $('a').click(function() {
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - 50
+            }, 500);
+            return false;
+        });
     });
-    $('a').click(function() {
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top - 50
-        }, 500);
-        return false;
-    });
-});
