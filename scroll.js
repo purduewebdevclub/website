f6b4de5dcc04784fabe9fc316d033145c480e193
Maneesh Tewani <mtewani@purdue.edu>
2016-02-26 10:38:48 -0500

    Revert

--- scroll.js
+++ scroll.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-        
-        $('a').click(function() {
-            $('html, body').animate({
-                scrollTop: $($.attr(this, 'href')).offset().top - 50
-            }, 500);
-            return false;
-        });
+$(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() !== 0) {
+            var nav = $(".navbar");
+            nav.css("background-color", "white");
+            $("a.white").removeClass("white").addClass("gray");
+        } else {
+            var nav = $(".navbar");
+            nav.css("background-color", "transparent");
+            $("a.gray").removeClass("gray").addClass("white");
+        }
     });
+    $('a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 50
+        }, 500);
+        return false;
+    });
+});
